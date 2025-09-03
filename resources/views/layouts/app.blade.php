41968952d15b38465388c86329dc7a8d3f36ed31
lawalvct@gmail.com <lawalthb@gmail.com>
2025-09-03 22:32:48 +0100

    Update app layout with enhanced meta tags and favicon

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,8 +4,14 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>@yield('title', 'Egbe Arobayo')</title>
+    <title>@yield('title', 'Egbe Arobayo Akinle Ijebu')</title>
     <meta name="description" content="@yield('meta_description', 'Egbe Arobayo - Preserving Yoruba Culture and Tradition')">
+   <link rel="icon" type="image/x-icon" href="{{ asset('images/icon.png') }}">
+<link rel="icon" type="image/png" sizes="32x32" href="{{ asset('images/icon.png') }}">
+ <!-- WhatsApp specific meta tags -->
+    <meta property="og:image:type" content="image/jpeg">
+    <meta name="theme-color" content="#2596be">
+  <meta property="og:image" content="@yield('og_image', asset('images/icon.png'))">
 
     <!-- Bootstrap CSS -->
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
